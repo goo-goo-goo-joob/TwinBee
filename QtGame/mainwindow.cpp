8c9e8f700689bfa900a28d6af5f075aae9d10a32
Unknown <pro1895sto@gmail.com>
2019-03-07 00:23:54 +0300

    Change keyboard actions

--- QtGame/mainwindow.cpp
+++ QtGame/mainwindow.cpp
@@ -1,8 +1,8 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-#include "Game.h"
+
 #include "observer.h"
-#include "bulletgen.h"
+
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -30,6 +30,8 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+
 void MainWindow::paintEvent(QPaintEvent *event)
 {
     QStyleOption opt;
@@ -42,67 +44,12 @@ void MainWindow::paintEvent(QPaintEvent *event)
 
 void MainWindow::keyPressEvent(QKeyEvent * event)
 {
-    Game& game = Game::Instance();
-
-    switch(event->key()){
-
-    case Qt::Key_Up:
-        game.bee->up = true;
-        break;
-
-    case Qt::Key_Right:
-        game.bee->right = true;
-        break;
-
-    case Qt::Key_Left:
-        game.bee->left = true;
-        break;
-
-    case Qt::Key_Down:
-        game.bee->down = true;
-        break;
-
-    case Qt::Key_Space:
-       //if(!event->isAutoRepeat()){
-            BulletGen& gen = BulletGen::Instance();
-            gen.prev = gen.on;
-            gen.on = true;
-            gen.Update(Notifer::Instance());
-        //}
-        break;
-    }
-    game.bee->Move();
-    update();
+    keysPressed.insert(event->key());
+    return;
 }
 
 void MainWindow::keyReleaseEvent(QKeyEvent *event){
 
-    Game& game = Game::Instance();
-
-    switch(event->key()){
-
-    case Qt::Key_Up:
-        game.bee->up = false;
-        break;
-
-    case Qt::Key_Right:
-        game.bee->right = false;
-        break;
-
-    case Qt::Key_Left:
-        game.bee->left = false;
-        break;
-
-    case Qt::Key_Down:
-        game.bee->down = false;
-        break;
-
-    case Qt::Key_Space:
-        BulletGen& gen = BulletGen::Instance();
-        //if(!event->isAutoRepeat()){
-            gen.on = false;
-        //}
-        break;
-    }
-    game.bee->Move();
+    keysPressed.remove(event->key());
+    return;
 }
--- QtGame/mainwindow.h
+++ QtGame/mainwindow.h
@@ -3,6 +3,8 @@
 #include <QKeyEvent>
 #include "GameItem.h"
 #include "observer.h"
+#include "Game.h"
+#include "bulletgen.h"
 
 namespace Ui {
 class MainWindow;
@@ -17,12 +19,24 @@ public:
     ~MainWindow();
     void paintEvent(QPaintEvent *event);
     void Update(const Notifer& n) {
+        Game& game = Game::Instance();
+        game.bee->up = keysPressed.contains(Qt::Key_Up);
+        game.bee->right = keysPressed.contains(Qt::Key_Right);
+        game.bee->left = keysPressed.contains(Qt::Key_Left);
+        game.bee->down = keysPressed.contains(Qt::Key_Down);
+
+         BulletGen& gen = BulletGen::Instance();
+         gen.on = keysPressed.contains(Qt::Key_Space);
+         gen.Update(Notifer::Instance());
+
+        game.bee->Move();
+
         update();
     }
 private:
     Ui::MainWindow *ui;
     int shootID = 0;
-    QSet<Qt::Key> keysPressed;
+    QSet<int> keysPressed;
 public slots:
     void keyReleaseEvent(QKeyEvent*);
     void keyPressEvent(QKeyEvent*);
