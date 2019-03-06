efe2684d813ff5e6a06761b639fdacdffcd1b2d4
Unknown <pro1895sto@gmail.com>
2019-03-07 00:28:32 +0300

    Fix freq

--- QtGame/bulletgen.h
+++ QtGame/bulletgen.h
@@ -6,7 +6,7 @@ class BulletGen{
 public:
     bool on;
 
-    static const int shootTime = 6000;
+    static const int shootTime = 15;
     static BulletGen& Instance()
     {
         static BulletGen b;
